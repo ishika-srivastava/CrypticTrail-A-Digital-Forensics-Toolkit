4148b5d9f8a69b2d423055171b55b31171a5a897
Ishika <ishika02908@gmail.com>
2024-07-12 23:40:23 +0530

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,11 +1,22 @@
 def printBanner():
-    print("""
-  ______                    __   __        _______              __ __ 
- |      |.----.--.--.-----.|  |_|__|.----.|_     _|.----.---.-.|__|  |
- |   ---||   _|  |  |  _  ||   _|  ||  __|  |   |  |   _|  _  ||  |  |
- |______||__| |___  |   __||____|__||____|  |___|  |__| |___._||__|__|
-              |_____|__|                                              
-                                                                                                                                                                                                                                                 
-""")
+  banner = """
+   ______                    __   __        _______              __ __ 
+  |      |.----.--.--.-----.|  |_|__|.----.|_     _|.----.---.-.|__|  |
+  |   ---||   _|  |  |  _  ||   _|  ||  __|  |   |  |   _|  _  ||  |  |
+  |______||__| |___  |   __||____|__||____|  |___|  |__| |___._||__|__|
+               |_____|__|                                              
+  """
+  copyright_info = """
+-------------------------------------------------
+CrypticTrail - A Digital Forensic Toolkit
+Created by: Ishika Srivastava
+GitHub : https://github.com/ishika-srivastava/CrypticTrail-A-Digital-Forensics-Toolkit
+Version: 1.0
+License: MIT
+(c) 2024 Ishika Srivastava. All rights reserved.
+-------------------------------------------------
+"""
+  print(banner)
+  print(copyright_info)
+
     
-printBanner()
